32fa71322792f9eab99bdf6608c28fc5b895e340
acensia <acensia@naver.com>
2023-05-22 22:03:51 +0900

    calender init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Works from "./elems/Works";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="Header">Header</header>
+      <div className="Container">
+        <div className="LeftSection">
+          <div className="RectangleBox">
+            <Works />
+          </div>
+        </div>
+        <div className="MiddleSection">
+          <div className="RectangleBox">Mid Section</div>
+        </div>
+        <div className="RightSection">
+          <div className="RectangleBox">Right Section</div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/elems/Works.js
@@ -0,0 +1,59 @@
+import TextTransition, { presets } from "react-text-transition";
+import React from "react";
+import "./elems.css";
+
+let today = new Date();
+let [month, week] = dateParser(today);
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function dateParser(d) {
+  let month = d.getMonth() + 1;
+  let date = d.getDate();
+  let day = d.getDay();
+  let week = parseInt((date - day + 1) / 7) + 1;
+  return [month, week];
+}
+
+function Works() {
+  const [month_, setMonth] = React.useState(month);
+  const [week_, setWeek] = React.useState(week);
+  const [block, setblock] = React.useState(false);
+
+  function clicked(e) {
+    if (block != true) {
+      setblock(true);
+      if (e.target.innerText === "◁") {
+        today.setDate(today.getDate() - 7);
+      } else {
+        today.setDate(today.getDate() + 7);
+      }
+      [month, week] = dateParser(today);
+      setMonth(month);
+      setWeek(week);
+      setTimeout(() => {
+        setblock(false);
+      }, 250);
+    }
+  }
+  return (
+    <div className="work">
+      <div className="dateSwipe">
+        <button onClick={clicked} disabled={block}>
+          ◁
+        </button>
+        <TextTransition springConfig={presets.wobbly}>{month_}</TextTransition>
+        <text>월</text>
+        <TextTransition springConfig={presets.wobbly}>{week_}</TextTransition>
+        <text>주차</text>
+        <button onClick={clicked} disabled={block}>
+          ▷
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Works;
