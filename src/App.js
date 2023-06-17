ce194b177660a92099acaa27b474a69a0cf49748
acensia <acensia@naver.com>
2023-06-17 20:21:12 +0900

    min pixels

--- src/App.js
+++ src/App.js
@@ -8,24 +8,36 @@ function App() {
   const handleLog = (bool) => {
     setLogged(!bool);
   };
+  const [full, checkFull] = useState(document.fullscreenElement !== null);
+
   if (logged) return <Log logged={handleLog} />;
   else
     return (
       <div className="App">
         <header className="Header">Header</header>
-        <div className="Container">
-          <div className="LeftSection">
-            <div className="RectangleBox">
-              <Works />
+        <div className="MainThree">
+          <div className="Container">
+            <div className="LeftSection">
+              <div className="RectangleBox">
+                <Works />
+              </div>
+            </div>
+            <div className="MiddleSection">
+              <div className="RectangleBox">
+                Mid Section
+                <div>
+                  {window.innerWidth}, {window.innerHeight} <br />
+                  {full ? "It's full" : "It's not full"} <br />
+                  {document.fullscreenElement}
+                </div>
+              </div>
+            </div>
+            <div className="RightSection">
+              <div className="RectangleBox">Right Section</div>
             </div>
           </div>
-          <div className="MiddleSection">
-            <div className="RectangleBox">Mid Section</div>
-          </div>
-          <div className="RightSection">
-            <div className="RectangleBox">Right Section</div>
-          </div>
         </div>
+        <div className="Under"></div>
       </div>
     );
 }
